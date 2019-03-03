0ff3634dfeb7ff73db6f7a2bd8b8b49d0efee04b
acs247 <47933177+acs247@users.noreply.github.com>
2019-03-02 18:57:40 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 // /**
 //  * WARRIOR Code
-//  * Last updated: 3/2/2019 at 14:41 by Amanda Steffy
+//  * Last updated: 3/2/2019 at 18:57 by Amanda Steffy
 //  * */
 
 /*
@@ -38,6 +38,7 @@ uint8_t testForNObstacle();
 void respToNObstacle();
 uint8_t testForEObstacle();
 void respToEObstacle();
+uint8_t testForMunitionButton();
 void respToMunitionButton();
 //Crossroads testing and response
 uint8_t testForCenter();
@@ -88,7 +89,6 @@ int US_L_ECHO_Pin = 23;
 //Motor Settings
 int maxSpeedMotor = 255; //HELP! SHOULD THIS BE 200? Letti got it to work at 200 but not higher
 int stopSpeedMotor = 0;
-//int shooterDirState = LOW;
 
 /*---------------WARRIOR Main Functions----------------*/
 void setup() {
@@ -126,7 +126,7 @@ void setup() {
 }
 
 void loop() {
-  /*
+  
   //Readout of the Ultrasonic sensors
   int distance_F = readUS_F();
   Serial.print("Front: ");
@@ -143,42 +143,54 @@ void loop() {
   int distance_L = readUS_L();
   Serial.print("Left: ");
   Serial.println(distance_L);
+  delay(500);
+  
+  //Drivetrain Testing
+  /*
+  driveW();
+  delay(500);
+  stopMotors();
+  delay(500);
+  driveN();
+  delay(500);
+  stopMotors();
+  delay(500);
+  driveE();
+  delay(500);
+  stopMotors();
+  delay(500);
+  driveS();
+  delay(500);
+  stopMotors();
   delay(500);*/
   
-  //Testing in the loop
-
   //Switch statement for states
   switch (state) {
     case driving_to_munition_button_from_throne_room:
       switch (sub_state) {
         case drivingW:
-          //TODO: test driveW
           driveW();
           if (testForWObstacle()) respToWObstacle();
           break;
         case drivingNArmoury:
-          //TODO: test driveN
           driveN();
-          if (testForNObstacle()) respToMunitionButton();
+          if (testForMunitionButton()) respToMunitionButton();
           break;
         default:
           Serial.println("I'm stuck in a driving_to_munition_button_from_throne_room nested state!");
       }
       break;
     case stopped:
-      //TODO: test stopMotors
       stopMotors();
       if (testForMunitionTimer()) respToMunitionTimer();
       break;
     case driving_to_crossroads:
       switch (sub_state) {
         case drivingE:
-          //TODO: test driveE
           driveE();
           if (testForEObstacle()) respToEObstacle();
           break;
         case drivingS:
-          //TODO: test driveS
           driveS();
 	        if (testForCenter()) respToCenter();
           break;
@@ -187,7 +199,6 @@ void loop() {
       }
       break;
     case shooting:
-      //TODO: test stopMotors
       stopMotors();
       startShooter();
       if (testForShooterTimer()) respToShooterTimer();
@@ -195,19 +206,16 @@ void loop() {
     case driving_to_munition_button_from_crossroads:
       switch (sub_state) {
         case drivingN:
-          //TODO: test driveN
           driveN();
 		      if (testForNObstacle()) respToNObstacle();
           break;
         case drivingW:
-          //TODO: test driveW
           driveW();
           if (testForWObstacle()) respToWObstacle();
           break;
         case drivingNArmoury:
-          //TODO: test driveN
           driveN();
-          if (testForNObstacle()) respToMunitionButton();
+          if (testForMunitionButton()) respToMunitionButton();
           break;
         default:
           Serial.println("I'm stuck in a driving_to_munition_button_from_crossroads nested state!");
@@ -222,9 +230,9 @@ void loop() {
 float readUS_F(){
   // Trigger sensor to send pulse
   digitalWrite(US_F_TRIG_Pin, LOW);
-  delayMicroseconds(5); //Note: blocking code
+  delayMicroseconds(5); 
   digitalWrite(US_F_TRIG_Pin, HIGH);
-  delayMicroseconds(10); //Note: blocking code
+  delayMicroseconds(10); 
   digitalWrite(US_F_TRIG_Pin, LOW);
 
   //Read the feedback
@@ -240,9 +248,9 @@ float readUS_F(){
 float readUS_B(){
   // Trigger sensor to send pulse
   digitalWrite(US_B_TRIG_Pin, LOW);
-  delayMicroseconds(5); //Note: blocking code
+  delayMicroseconds(5); 
   digitalWrite(US_B_TRIG_Pin, HIGH);
-  delayMicroseconds(10); //Note: blocking code
+  delayMicroseconds(10); 
   digitalWrite(US_B_TRIG_Pin, LOW);
 
   //Read the feedback
@@ -276,9 +284,9 @@ float readUS_R(){
 float readUS_L(){
   // Trigger sensor to send pulse
   digitalWrite(US_L_TRIG_Pin, LOW);
-  delayMicroseconds(5); //Note: blocking code
+  delayMicroseconds(5); 
   digitalWrite(US_L_TRIG_Pin, HIGH);
-  delayMicroseconds(10); //Note: blocking code
+  delayMicroseconds(10);
   digitalWrite(US_L_TRIG_Pin, LOW);
 
   //Read the feedback
@@ -292,35 +300,30 @@ float readUS_L(){
 }
 
 void driveW(){
-  //TODO: test
   digitalWrite(dirPin1, LOW);
   digitalWrite(dirPin2, HIGH);
   analogWrite(pwmNorthSouth, stopSpeedMotor);
   analogWrite(pwmEastWest, maxSpeedMotor); 
 }
 void driveN(){
-  //TODO: test
   digitalWrite(dirPin1, HIGH);
   digitalWrite(dirPin2, LOW);
   analogWrite(pwmNorthSouth, maxSpeedMotor);
   analogWrite(pwmEastWest, stopSpeedMotor); 
 }
 void driveS(){
-  //TODO: test
   digitalWrite(dirPin1, LOW);
   digitalWrite(dirPin2, HIGH);
   analogWrite(pwmNorthSouth, maxSpeedMotor);
   analogWrite(pwmEastWest, stopSpeedMotor); 
 }
 void driveE(){
-  //TODO: test
   digitalWrite(dirPin1, HIGH);
   digitalWrite(dirPin2, LOW);
   analogWrite(pwmNorthSouth, stopSpeedMotor);
   analogWrite(pwmEastWest, maxSpeedMotor); 
 }
 void stopMotors(){
-  //TODO: test
   analogWrite(pwmNorthSouth, stopSpeedMotor);
   analogWrite(pwmEastWest, stopSpeedMotor);
 }
@@ -333,6 +336,7 @@ uint8_t testForWObstacle() {
 
 void respToWObstacle() {
   Serial.println("W Obstacle Detected!");
+  stopMotors();
   sub_state = drivingNArmoury;
 }
 
@@ -344,6 +348,7 @@ uint8_t testForNObstacle() {
 
 void respToNObstacle() {
   Serial.println("N Obstacle Detected!");
+  stopMotors();
   sub_state = drivingW;
 }
 
@@ -354,10 +359,23 @@ uint8_t testForEObstacle() {
 }
 void respToEObstacle() {
   Serial.println("E Obstacle Detected!");
+  stopMotors();
   sub_state = drivingS;
+}
+uint8_t testForMunitionButton() {
+  float US_F = readUS_F();
+  float US_B = readUS_B();
+  if ((US_B > 200) && (US_F < 5)) {
+    driveN();
+    delay(200); //blocking code
+    return 1;
+  }
+  else return 0;
 }
 void respToMunitionButton() {
   munition_timer.reset();
+  driveS();
+  delay(300); //blocking code
   state = stopped;
   Serial.println("Responding to Munition Button!");
 }
@@ -375,6 +393,7 @@ void respToCenter() {
 void startShooter() {
   //TODO: test
   Serial.println("FIRE!");
+  //TODO: add release code for the release mechanism
   digitalWrite(shooter_dir1, HIGH);
   digitalWrite(shooter_dir2, HIGH);
   analogWrite(shooter_enable1, maxSpeedMotor);
@@ -392,7 +411,6 @@ uint8_t testForShooterTimer() {
   return (uint8_t) shooter_timer.check();
 }
 void respToShooterTimer() {
-  //TODO: test
   Serial.println("Shooting timer has expired!");
   digitalWrite(shooter_dir1, LOW);
   digitalWrite(shooter_dir2, LOW);
